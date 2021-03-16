63030b6946b54750029e5920dcdfedc3cdb5df9f
João Alves <joao.alves@liberali.local>
2021-03-16 12:48:47 -0400

    Commit inicial e atribuição da clase employee

---
+++ HerancaEPolimorfismo/Entites/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerancaEPolimorfismo.Entites
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee() { }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
